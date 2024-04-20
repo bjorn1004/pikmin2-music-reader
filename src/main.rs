56c1fe65a9aa2f199d51f753cb12cf74bd85b9fa
bjorn1004 <bjorn1004@gmail.com>
2024-04-21 01:34:58 +0200

    Added tracks

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 use std::{
     error::Error,
     fs::File,
-    io::Read,
+    io::{BufReader, Read, Seek, SeekFrom},
     num::NonZeroU8,
     path::PathBuf,
     sync::mpsc::{channel, Receiver},
     thread,
-    time::Duration,
 };
 
 use eframe::{
-    egui::{mutex::Mutex, CentralPanel},
+    egui::{CentralPanel, CollapsingHeader, Grid, ScrollArea},
     run_native, App,
 };
 
@@ -45,6 +44,14 @@ impl App for Main {
                 ui.heading("Pikmin 2 Music Reader");
             });
             ui.separator();
+            if ui.button("Open conductor file…").clicked() {
+                if let Some(path) = rfd::FileDialog::new()
+                    .add_filter("Conductor files", &["cnd"])
+                    .pick_file()
+                {
+                    self.set_conductor_path(Some(path));
+                }
+            }
             // Check if there's a conductor path selected
             if let Some(conductor_path) = &self.conductor_path {
                 // Show conductor path
@@ -54,11 +61,109 @@ impl App for Main {
                 });
                 // Check if there's a Conductor struct in memory
                 if let Some(conductor) = &self.conductor {
+                    static INVALID_FILE_ERROR: &str = "Invalid File";
+                    ui.heading(
+                        conductor_path
+                            .file_name()
+                            .expect(INVALID_FILE_ERROR)
+                            .to_str()
+                            .expect(INVALID_FILE_ERROR),
+                    );
                     // Show Conductor
-                    ui.label("success!");
-                    ui.horizontal_top(|ui| {
-                        ui.label("Bpm:");
-                        ui.monospace(conductor.bpm.to_string());
+                    Grid::new("conductor_metadata")
+                        .num_columns(2)
+                        .show(ui, |ui| {
+                            ui.label("Louie swing:");
+                            ui.monospace(conductor.louie_swing.to_string());
+                            ui.end_row();
+
+                            ui.label("Bpm:");
+                            ui.monospace(conductor.bpm.to_string());
+                            ui.end_row();
+
+                            ui.label("Track count:");
+                            ui.monospace(conductor.track_count.to_string());
+                            ui.end_row();
+                        });
+                    ui.separator();
+                    ui.heading("Tracks");
+                    ScrollArea::vertical().show(ui, |ui| {
+                        for (track_id, track) in conductor.tracks.iter().enumerate() {
+                            CollapsingHeader::new(track.description())
+                                .id_source(format!("{}_{track_id}", track.description()))
+                                .show(ui, |ui| {
+                                    Grid::new(format!("{}_grid", track.description()))
+                                        .num_columns(2)
+                                        .show(ui, |ui| {
+                                            ui.label("Volume:");
+                                            ui.monospace(track.volume.to_string());
+                                            ui.end_row();
+
+                                            ui.label("Panning:");
+                                            ui.monospace(track.panning.to_string());
+                                            ui.end_row();
+
+                                            ui.label("Track copy:");
+                                            ui.monospace(track.track_copy.to_string());
+                                            ui.end_row();
+
+                                            ui.label("Initial Delay:");
+                                            ui.monospace(track.init_delay.to_string());
+                                            ui.end_row();
+
+                                            ui.label("Echo:");
+                                            ui.monospace(track.echo.to_string());
+                                            ui.end_row();
+
+                                            ui.label("Ordered:");
+                                            ui.monospace(track.ordered.to_string());
+                                            ui.end_row();
+
+                                            ui.label("Bank:");
+                                            ui.monospace(match track.bank {
+                                                Bank::Pikmin1SFX => "Pikmin 1 SFX",
+                                                Bank::WatanabeSFX => "Watanabe SFX",
+                                                Bank::TotakaSFX => "Totaka SFX",
+                                                Bank::HikinoSFX => "Hikino SFX",
+                                                Bank::WakaiInstruments => "Wakai Instruments",
+                                                Bank::TotakaInstruments => "Totaka Instruments",
+                                            });
+                                            ui.end_row();
+
+                                            ui.label("Program:");
+                                            ui.monospace(track.program.to_string());
+                                            ui.end_row();
+
+                                            ui.label("Gesture set:");
+                                            ui.monospace(track.gesture_set.to_string());
+                                            ui.end_row();
+
+                                            ui.label("Timing ruleset:");
+                                            ui.monospace(track.timing.to_string());
+                                            ui.end_row();
+
+                                            ui.label("Gesture count:");
+                                            ui.monospace(track.gesture_count.to_string());
+                                            ui.end_row();
+
+                                            ui.label("Silent count:");
+                                            ui.monospace(track.silent_count.to_string());
+                                            ui.end_row();
+
+                                            ui.label("Transposition:");
+                                            ui.monospace(track.transposition.to_string());
+                                            ui.end_row();
+
+                                            ui.label("B offset flag:");
+                                            ui.monospace(track.b_offset_flag.to_string());
+                                            ui.end_row();
+
+                                            ui.label("Q offset flag:");
+                                            ui.monospace(track.q_offset_flag.to_string());
+                                            ui.end_row();
+                                        })
+                                });
+                        }
                     });
                 // No Conductor in memory, check if we're currently waiting on the parsing thread
                 } else if let Some(reciever) = &self.conductor_channel {
@@ -81,7 +186,13 @@ impl App for Main {
                     let ctx = ctx.clone();
 
                     thread::spawn(move || -> Result<(), Box<dyn Error + Send + Sync>> {
-                        let conductor = Conductor::from_file(&file_path_clone)?;
+                        let conductor = match Conductor::from_file(&file_path_clone) {
+                            Ok(conductor) => conductor,
+                            Err(e) => {
+                                println!("{}", e);
+                                return Err(e);
+                            }
+                        };
                         let send = sender.send(conductor);
                         ctx.request_repaint();
                         Ok(send?)
@@ -90,14 +201,6 @@ impl App for Main {
             } else {
                 // No file selected
                 ui.label("Drag a .cnd file into the window to read it:");
-            }
-            if ui.button("Open conductor file…").clicked() {
-                if let Some(path) = rfd::FileDialog::new()
-                    .add_filter("Conductor files", &["cnd"])
-                    .pick_file()
-                {
-                    self.set_conductor_path(Some(path));
-                }
             }
         });
     }
@@ -130,22 +233,85 @@ pub struct Conductor {
 
 impl Conductor {
     pub fn from_file(file: &PathBuf) -> Result<Self, Box<dyn Error + Send + Sync>> {
-        let mut file = File::open(file)?;
+        let file = File::open(file)?;
+        let mut reader = BufReader::new(file);
         let mut metadata_buffer: [u8; 3] = [0; 3];
-        file.read_exact(&mut metadata_buffer)?;
+        reader.read_exact(&mut metadata_buffer)?;
         let [louie_swing, bpm, track_count] = metadata_buffer;
-        // simulate work
-        thread::sleep(Duration::from_secs(2));
-        // TODO: actually parse the file
+
+        // seek to the first byte of the first track
+        reader.seek(SeekFrom::Current(21))?;
+        let mut tracks = Vec::with_capacity(track_count.into());
+        let mut byte_buffer: [u8; 4 * 9] = [0; 4 * 9];
+        for i in 0..track_count {
+            // read data block
+            reader.read_exact(&mut byte_buffer)?;
+            // extract initial data
+            let ([_, init_delay, b_offset_flag], remain) = byte_buffer.split_at(3) else {
+                return Err("Error parsing (1)".into());
+            };
+            // extract description
+            let (description_buffer, remain) = remain.split_at(8);
+            let mut description: [u8; 8] = [0; 8];
+            description.copy_from_slice(description_buffer);
+            // extract track_copy and echo
+            let ([track_copy, echo], remain) = remain.split_at(2) else {
+                return Err("Error parsing (2)".into());
+            };
+            // seek 8 bytes forwards
+            let (_, remain) = remain.split_at(8);
+            // extract remainder
+            let [ordered, bank_byte, program, _, gesture_set, _, timing, gesture_count, silent_count, _, transposition, volume, panning, q_offset_flag, _] =
+                remain
+            else {
+                return Err("Error parsing (3)".into());
+            };
+            let bank = match bank_byte {
+                0 => Ok(Bank::Pikmin1SFX),
+                1 => Ok(Bank::WatanabeSFX),
+                2 => Ok(Bank::TotakaSFX),
+                3 => Ok(Bank::HikinoSFX),
+                4 => Ok(Bank::WakaiInstruments),
+                5 => Ok(Bank::TotakaInstruments),
+                _ => Err(format!("Error parsing Bank for track {}", i + 1)),
+            }?;
+            tracks.push(Track {
+                init_delay: *init_delay,
+                b_offset_flag: (*b_offset_flag == 1),
+                q_offset_flag: (*q_offset_flag == 1),
+                description,
+                track_copy: *track_copy,
+                echo: *echo,
+                ordered: (*ordered == 1),
+                bank,
+                program: *program,
+                gesture_set: *gesture_set,
+                timing: *timing,
+                gesture_count: *gesture_count,
+                silent_count: *silent_count,
+                transposition: *transposition as i8,
+                volume: *volume,
+                panning: {
+                    let panning_i8: i16 = 64_i16.wrapping_add((*panning).into());
+                    panning_i8 as i8
+                },
+            });
+            match reader.seek(SeekFrom::Current(6 * 4)) {
+                Ok(_) => (),
+                Err(_) => break,
+            };
+        }
+
         Ok(Self {
             louie_swing,
             bpm,
             track_count: NonZeroU8::new(track_count).unwrap(),
-            tracks: Vec::new(),
+            tracks,
         })
     }
 }
 
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Track {
     pub init_delay: u8,
     pub b_offset_flag: bool,
@@ -167,10 +333,18 @@ pub struct Track {
 
 impl Track {
     pub fn description(&self) -> &str {
-        std::str::from_utf8(&self.description).unwrap_or("!!Description string is corrupted!!")
+        // 205 is apparently the terminating character
+        let terminated_string = &self
+            .description
+            .split(|n| *n == 205)
+            .next()
+            // if this happens, it means the Description is invalid
+            .unwrap_or(&[205]);
+        std::str::from_utf8(terminated_string).unwrap_or("!!Description string is corrupted!!")
     }
 }
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum Bank {
     Pikmin1SFX,
     WatanabeSFX,
